2b0cb204eda440d2705519cc2256a66331f38988
RoyerSA007 <rogeasa@outlook.es>
2025-06-04 13:22:13 -0600

    Final

--- src/screen/DetailProduct.js
+++ src/screen/DetailProduct.js
@@ -1,88 +1,120 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Button } from "react-native"
-import Icon from 'react-native-vector-icons/Ionicons';
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, ScrollView } from "react-native";
 
-export default function DetailProducts ({route, navigation}) {  
+export default function DetailProducts({ route, navigation }) {
     const { products } = route.params;
     const [esFavorito, setEsFavorito] = useState(false);
 
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.titleContainer}>
-                <View style={{ flex: 1 }}>
-                    <Text style={styles.titleTexto}>{products.title}</Text>
-                </View>
-                <TouchableOpacity onPress={() => setEsFavorito(!esFavorito)} style={styles.corazonContainer}>
-                    <Text style={styles.corazon}> {esFavorito ? "❤️" : "🤍"} </Text>
+            <ScrollView contentContainerStyle={styles.content}>
+            <View style={styles.header}>
+                <Text style={styles.title}>{products.title}</Text>
+                <TouchableOpacity onPress={() => setEsFavorito(!esFavorito)}>
+                    <Text style={styles.heart}>{esFavorito ? "❤️" : "🤍"}</Text>
                 </TouchableOpacity>
             </View>
-            <Image source={{ uri: products.image }} style={styles.image}/>
-            <Text>{products.price}</Text>
-            <Text>{products.description}</Text>
-            <Text>{products.category}</Text>
-            <Text>{products.rating.rate} ({products.rating.count})</Text>
-            <Button title="Agregar al carrito" onPress={() => navigation.navigate('Carrito')}/>
-            <Button title="Catalogo" onPress={() => navigation.goBack()}/>
-        </SafeAreaView>
-    );
+
+            <Image source={{ uri: products.image }} style={styles.image} resizeMode="contain" />
+            <Text style={styles.price}>${products.price.toFixed(2)}</Text>
+            <Text style={styles.category}>{products.category.toUpperCase()}</Text>
+            <Text style={styles.description}>{products.description}</Text>
+            <Text style={styles.rating}>⭐ {products.rating.rate} ({products.rating.count} reviews)</Text>
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => navigation.navigate("Carrito")} >
+                    <Text style={styles.buttonText}>Agregar al carrito</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.button, styles.outlinedButton]}
+                    onPress={() => navigation.goBack()}>
+                    <Text style={[styles.buttonText, styles.outlinedButtonText]}>Volver al catálogo</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 50,
-        backgroundColor: "#00a1fb"
-    },
-    cardContainer: {
-        backgroundColor: "#c1c1c1",
-        borderRadius: 10,
-        padding: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 20
-    },
-    image: {
-        height: 200,
-        width: 200,
-
-    },
-    errorStyle: {
-        color: "red",
-        fontSize: 10
-    },
-    card: {
-        marginBottom: 20,
-        backgroundColor: '#f8f8f8',
-        borderRadius: 10,
-        overflow: 'hidden',
-        borderColor: '#ddd',
-        borderWidth: 1
-    },
-    title:{
-        fontSize: 18,
-        fontWeight: 'bold',
-        padding: 10 
-    },
-    titleContainer: {
-        flexDirection: "row",
-        alignItems: "flex-start",
-        justifyContent: "space-between",
-        width: "100%",
-        marginBottom: 10,
-        alignItems: "center",
-    },
-    titleTexto: {
-        fontSize: 20,
-        fontWeight: "bold",
-        color: "#fff",
-    },
-    corazonContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    corazon: {
-        fontSize: 24,
-    }
-})
+  container: {
+    flex: 1,
+    marginBottom: 40,
+    backgroundColor: "#f9f9f9",
+  },
+  content: {
+    padding: 20,
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  title: {
+    flex: 1,
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#333",
+    marginRight: 10,
+  },
+  heart: {
+    fontSize: 28,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    marginBottom: 20,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  price: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#100aff",
+    marginBottom: 10,
+  },
+  category: {
+    fontSize: 14,
+    color: "#777",
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 16,
+    color: "#444",
+    textAlign: "justify",
+    marginBottom: 20,
+  },
+  rating: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 30,
+  },
+  buttonsContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  button: {
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "#007aff",
+    marginBottom: 15,
+    alignItems: "center",
+  },
+  outlinedButton: {
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#007aff",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  outlinedButtonText: {
+    color: "#007aff",
+  },
+});
--- src/screen/ProductListinScreen.js
+++ src/screen/ProductListinScreen.js
@@ -1,96 +1,125 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Dimensions} from "react-native";
 
 export default function ProductListinScreen({ navigation }) {
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        getProduct();
-    }, []);
+  useEffect(() => {
+    getProduct();
+  }, []);
 
-    const getProduct = () => {
-        const URL = "https://fakestoreapi.com/products";
+  const getProduct = () => {
+    const URL = "https://fakestoreapi.com/products";
 
-        fetch(URL)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("Algo malo ha pasado en la conexión");
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setProducts(data);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.log(error.message);
-                setError(error.message);
-                setIsLoading(false);
-            });
-    };
+    fetch(URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Algo malo ha pasado en la conexión");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+        setIsLoading(false);
+      });
+  };
 
-    const renderProduct = ({ item }) => (
-        <TouchableOpacity
-            style={styles.cardContainer}
-            onPress={() => navigation.navigate('ProductsDetail', { products: item })}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-            <Text style={styles.title}>
-                {item.title}
-            </Text>
-        </TouchableOpacity>
-    );
+  const renderProduct = ({ item }) => (
+    <TouchableOpacity
+        style={styles.cardContainer}
+        onPress={() => navigation.navigate("ProductsDetail", { products: item })}>
+        
+        <Image source={{ uri: item.image }} style={styles.image} resizeMode="contain" />
+        <Text numberOfLines={2} style={styles.title}>{item.title}</Text>
+        <Text style={styles.price}>${item.price.toFixed(2)}</Text>
+    </TouchableOpacity>
+  );
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>Productos</Text>
-            {isLoading ? (
-                <ActivityIndicator color="red" size="large" />
-            ) : error ? (
-                <Text style={styles.errorStyle}>{error}</Text>
-            ) : (
-                <FlatList
-                    showsVerticalScrollIndicator={false}
-                    data={products}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={renderProduct}
-                />
-            )}
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.header}>Tienda</Text>
+        {isLoading ? (
+            <ActivityIndicator color="#007aff" size="large" />
+        ) : error ? (
+            <Text style={styles.errorStyle}>{error}</Text>
+        ) : (
+        <FlatList
+            showsVerticalScrollIndicator={false}
+            data={products}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderProduct}
+            contentContainerStyle={styles.productList}
+            
+            //numColumns={2}
+            //columnWrapperStyle={{ justifyContent: 'space-between' }}
+
+        />
+      )}
+    </SafeAreaView>
+  );
 }
 
+const { width } = Dimensions.get("window");
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
-        backgroundColor: "#00a1fb"
-    },
-    cardContainer: {
-        backgroundColor: "#c1c1c1",
-        borderRadius: 10,
-        padding: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 10
-    },
-    image: {
-        height: 200,
-        width: 200,
-    },
-    errorStyle: {
-        color: "white",
-        fontSize: 16,
-        textAlign: "center",
-        marginTop: 10
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        padding: 10,
-        textAlign: "center"
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    marginBottom: 40,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 16,
+    color: "#333",
+    alignSelf: "center",
+  },
+  productList: {
+    paddingBottom: 20,
+  },
+  cardContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    padding: 16,
+    marginVertical: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 4,
+    alignItems: "center",
+    //width: (width / 2) - 24,
+  },
+  image: {
+    width: width * 0.6,
+    height: 180,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#333",
+    marginBottom: 4,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#100aff",
+  },
+  errorStyle: {
+    color: "red",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 20,
+  },
 });
